901ce2e1320b9fb5d230874f31564434923479a3
patricia123-cell <patito261@hotmail.com>
2026-04-24 03:31:46 +0000

    fix:evitar registros duplicados

--- src/app.py
+++ src/app.py
@@ -62,7 +62,10 @@ def signup_for_activity(activity_name: str, email: str):
     # Get the specific activity
     activity = activities[activity_name]
 
-#validate student is not alredy signed up if email in activity["participants"]:raise HTTPException(status_code=400,detail="Student already registered")
+#validate student is not alredy signed up
+if email in activity["participants"]:
+    raise HTTPException(status_code=400,
+    detail="Student already registered")
 
     # Add student
     activity["participants"].append(email)
